21f16155cf822b1a40e9fd196c8c9adb4b57b98a
JyothiYellam <jyothiym23@gmail.com>
2026-09-01 13:15:32 +0530

    changes

--- 31-cicd-tools/main.tf
+++ 31-cicd-tools/main.tf
@@ -83,7 +83,7 @@ resource "aws_instance" "runner" {
 resource "aws_instance" "sonarqube" {
   count = var.sonar ? 1 : 0
   ami           = local.sonar_ami_id
-  instance_type = "t3.large"
+  instance_type = "t3.medium"
   vpc_security_group_ids = [local.sonar_sg_id]
   subnet_id = local.public_subnet_id #replace your Subnet in default VPC
   key_name = "Terraform"
